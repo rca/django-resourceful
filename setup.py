07ac1f390c215009aa6afd1cf80c3fe59fd50c76
Roberto Aguilar <roberto@baremetal.io>
2013-01-16 18:28:53 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-resourceful',
+      version='0.1',
+      description='Resourceful routing for Django',
+      author='Roberto Aguilar',
+      author_email='roberto.c.aguilar@gmail.com',
+      url='http://github.com/rca/django-resourceful',
+      packages=['resourceful'],
+     )
